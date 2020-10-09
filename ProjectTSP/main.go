f987ceab921498acb8f5e73d99b8b475ac30ac0a
Sebastián Delgado <sfdelgadop@unal.edu.co>
2020-10-08 23:46:20 -0500

    adding reading of tests

--- ProjectTSP/main.go
+++ ProjectTSP/main.go
@@ -4,16 +4,26 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"bufio"
+	"os"
+	"log"
+	"strconv"
+	"strings"
 )
 
-const indSize = 10
+var indSize int
 const popSize = 20
 const generations = 50
+var matrix [][]float64
 
 func testfunc(genome []int) float32 {
 	var rta float32 = 0.0
-	for i, value := range genome {
-		rta += float32(i * value)
+	for i :=0 ; i < len(genome) ; i++ {
+		if i < len(genome)-1{
+			rta += float32(matrix[genome[i]][genome[i+1]])
+		}else{
+			rta += float32(matrix[genome[i]][genome[0]])
+		}
 	}
 	return rta
 }
@@ -30,14 +40,48 @@ func initPopulation(function func([]int) float32) [popSize]Agent {
 func getBest(agents ...Agent) Agent {
 	best := agents[0]
 	for _, agent := range agents {
-		if agent.fitness > best.fitness {
+		if agent.fitness < best.fitness {
 			best = agent
 		}
 	}
 	return best
 }
 
+func chargeTest(){
+	file,err := os.Open("./setup/100.tsp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		indSize, err = strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		break
+	}
+	matrix = make([][]float64, indSize)
+	for i := 0; i < indSize; i++ {
+		matrix[i] = make([]float64, indSize)
+	}
+	j := 0
+	for scanner.Scan() {
+		arr := strings.Fields(scanner.Text())
+		for k := 0 ; k < len(arr) ; k++ {
+			temp,err := strconv.ParseFloat(arr[k],64)
+			if err != nil {
+				log.Fatal(err)
+			}	
+			matrix[j][k] = temp
+		}
+		j++
+	}
+}
+
 func main() {
+
+	chargeTest()
 	rand.Seed(time.Now().UnixNano())
 	population := initPopulation(testfunc)
 
